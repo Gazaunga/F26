5496de2f05511162696c429374a69d25c5f76d4b
Gazaunga <Gazaunga@gmail.com>
2017-08-01 17:41:09 -0400

    Update fedup.sh

--- .local/fedup.sh
+++ .local/fedup.sh
@@ -11,24 +11,75 @@ sudo dnf -y install steam
 sudo dnf -y install vlc
 sudo dnf -y install dropbox nautilus-dropbox
 sudo dnf -y install nano vim rpm-build
-sudo dnf -y install postgresql-server postgresql-contrib
+sudo dnf -y install postgresql-server postgresql-contrib memcached redis
 sudo systemctl enable postgresql
 sudo postgresql-setup --initdb --unit postgresql
+sudo sed -i.bak 's/ident/trust/' /var/lib/pgsql/data/pg_hba.conf # NEVER do this in production servers
 sudo dnf -y install phpPgAdmin pgadmin3
+#sudo systemctl start postgresql
+#sudo su - postgres
+#createuser gazbit -p
+#createdb gazbit --owner=gazbit
 # opera screen terminator youtube-dl ranger mc npm
-# ruby install
-cd ~
-wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
-tar -xzvf ruby-install-0.6.1.tar.gz
-cd ruby-install-0.6.1/
-sudo make install
+# Gimp and a 'photoshop' skin
+sudo dnf -y install gimp
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/doctormo/GimpPs/master/tools/install.sh)"
+# I should probably copy that gist somewhere
+# Spotify
+# sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
+# sudo dnf -y install spotify-client
+# Twitter
+sudo dnf -y install corebird
+sudo dnf -y install docker docker-compose
+# Latest java web browser plugins
+sudo dnf -y install icedtea-web
+# A few terminals
+sudo dnf copr enable heikoada/terminix
+sudo dnf -y install terminix terminator
+# Gnome Tweak tools
+sudo dnf -y install gnome-tweak-tool
 
-# check to see if it isntalled successfully
-ruby-install -v
 
-# install ruby
-ruby-install --system --latest ruby
-ruby-install jruby
+# Make Gnome titlebars less 'fat'
+tee ~/.config/gtk-3.0/gtk.css <<-EOF
+.header-bar.default-decoration {
+ padding-top: 3px;
+ padding-bottom: 3px;
+ font-size: 0.8em;
+}
+
+.header-bar.default-decoration .button.titlebutton {
+ padding: 0px;
+}
+EOF
+
+# Terminator Config
+touch ~/.config/terminator/config
+tee ~/.config/terminator/config <<-EOF
+[global_config]
+  title_transmit_bg_color = "#d30102"
+  focus = system
+[keybindings]
+[layouts]
+  [[default]]
+    [[[child1]]]
+      parent = window0
+      type = Terminal
+      profile = default
+    [[[window0]]]
+      parent = ""
+      type = Window
+[plugins]
+[profiles]
+  [[default]]
+    use_system_font = false
+    font = Hack 12
+    scrollback_lines = 2000
+    palette = "#073642:#dc322f:#859900:#b58900:#268bd2:#d33682:#2aa198:#eee8d5:#586e75:#cb4b16:#586e75:#657b83:#839496:#6c71c4:#93a1a1:#fdf6e3"
+    foreground_color = "#eee8d5"
+    background_color = "#002b36"
+    cursor_color = "#eee8d5"
+EOF
 
 # zsh Setup
 cd ~
@@ -104,3 +155,23 @@ curl -sLf https://spacevim.org/install.sh | bash
 # Axe Menu
 # Extend Panel Menu
 # Dash to Panel
+
+# ruby install
+cd ~
+wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
+tar -xzvf ruby-install-0.6.1.tar.gz
+cd ruby-install-0.6.1/
+sudo make install
+
+# check to see if it isntalled successfully
+ruby-install -v
+
+# install ruby
+ruby-install --system --latest ruby
+ruby-install jruby
+
+# Latest stable Ruby with RVM
+#sudo dnf -y install patch autoconf gcc-c++ patch libffi-devel automake libtool bison sqlite-devel ImageMagick-devel nodejs git gitg
+#curl -sSL https://rvm.io/mpapis.asc | gpg2 --import
+#curl -L https://get.rvm.io | bash -s stable --ruby
+#sudo npm -g install brunch phantomjs
